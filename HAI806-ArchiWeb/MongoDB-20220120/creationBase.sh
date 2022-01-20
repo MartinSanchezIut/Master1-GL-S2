ff5fc6a59e9c438ce6440794344a791c66349152
MartinSanchezIut <martin.sanchez.iut@gmail.com>
2022-01-20 17:59:17 +0100

    visiteur

---
+++ HAI806-ArchiWeb/MongoDB-20220120/creationBase.sh
@@ -0,0 +1,4 @@
+mongoimport --db NomDATABASE --collection users --file data/users.json --jsonArray --drop
+mongoimport --db NomDATABASE --collection produits --file data/produits.json --jsonArray --drop
+mongoimport --db NomDATABASE --collection recettes --file data/recettes.json --jsonArray --drop
+mongoimport --db NomDATABASE --collection avis --file data/avis.json --jsonArray --drop
